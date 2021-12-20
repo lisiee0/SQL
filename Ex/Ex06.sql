97b0c5482c7d635e0ea21fd1d3db380edd359941
Hojun_Yoo <lisiee0@naver.com>
2021-12-20 11:45:28 +0900

    12-20

--- Ex/Ex06.sql
+++ Ex/Ex06.sql
@@ -14,6 +14,22 @@ select  e.first_name, e.salary, e.department_id, d.department_id, d.department_n
 from    employees e left outer join departments d
 on      e.department_id= d.department_id;
 
+-- 오라클 표현
+select  count(*)
+from    employees e, departments d
+where   e.department_id= d.department_id(+);
+
+
+-- RIGHT OUTER JOIN
+select  e.first_name, e.salary, e.department_id, d.department_id, d.department_name
+from    employees e right outer join departments d
+on      e.department_id= d.department_id;
+
+-- 오라클 표현
+select  count(*)
+from    employees e, departments d
+where   e.department_id(+)= d.department_id;
+
 
 
 
