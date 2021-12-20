109f0f075ce6a04582aa95958d69a512d31080ab
Hojun_Yoo <lisiee0@naver.com>
2021-12-20 13:30:21 +0900

    12-20

--- Ex/Ex06.sql
+++ Ex/Ex06.sql
@@ -31,7 +31,20 @@ from    employees e, departments d
 where   e.department_id(+)= d.department_id;
 
 
+-- RIGHT OUTER JOIN --> LEFT OUTER JOIN
+select  e.first_name, e.salary, e.department_id, d.department_id, d.department_name
+from    departments d left outer join employees e
+on      d.department_id= e.department_id;
 
+-- 오라클 표현
+select  e.first_name, e.salary, e.department_id, d.department_id, d.department_name
+from    departments d, employees e
+where   d.department_id= e.department_id(+);
+
+-- full outer join
+select  *
+from    employees e full outer join departments d
+on      e.department_id= d.department_id;
 
 
 
