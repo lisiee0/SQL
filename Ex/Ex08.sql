0c6ebf3c377cf976fd8ad369bd27d1fe5eb59b39
Hojun_Yoo <lisiee0@naver.com>
2021-12-22 12:22:07 +0900

    12-22

--- Ex/Ex08.sql
+++ Ex/Ex08.sql
@@ -39,7 +39,7 @@ from   (select  first_name, salary
 where   rownum between 2 and 5; -- 아무것도 출력되지 않음
 
 -- 문제 해결
-select  first_name, salary
+select  r.rno, first_name, salary
 from    (select  rownum rno,
                  first_name, salary
          from   (select  first_name, salary
@@ -48,7 +48,48 @@ from    (select  rownum rno,
 where   rno between 2 and 5;                 
 
 
-       
-        
-        
+-- 예제
+-- 07년에 입사한 직원중 급여가 많은 직원중 3에서 7등의 이름 급여 입사일은? 
 
+-- 1단계. 2007년에 입사한 직원의 급여를 내림차순으로 정렬
+select  to_char(hire_date, 'YYYY'), first_name, salary
+from    employees
+where   to_char(hire_date, 'YYYY')= 2007
+order by salary desc;
+
+-- 2단계. 전 단계 테이블에 ROWNUM 부여
+select  rownum rno,
+        first_name, salary
+from    (select  to_char(hire_date, 'YYYY'), first_name, salary, hire_date
+         from    employees
+         where   to_char(hire_date, 'YYYY')= 2007
+         order by salary desc) o;
+         
+-- 3단계. 전 단계 테이블에서 급여 순위 3~7등의 이름 급여 입사일 출력 (최종)
+select  r.rno, first_name, salary, hire_date
+from    (select  rownum rno,
+                 first_name, salary, hire_date
+         from    (select  to_char(hire_date, 'YYYY'), first_name, salary, hire_date
+                  from    employees
+                  where   to_char(hire_date, 'YYYY')= 2007
+                  order by salary desc) o) r
+where   r.rno between 3 and 7;        
+
+
+-- 예제+
+-- 07년에 입사한 직원중 급여가 많은 직원중 3에서 7등의 이름, 급여, 입사일, 부서명 출력 
+select  r.rno, first_name, salary, hire_date, d.department_name
+from    (select  rownum rno,
+                 first_name, salary, hire_date, department_id
+         from    (select  to_char(hire_date, 'YYYY'), first_name, salary, hire_date, department_id
+                  from    employees
+                  where   to_char(hire_date, 'YYYY')= 2007
+                  order by salary desc) o) r,
+        departments d          
+where   r.department_id= d.department_id(+)
+and     r.rno between 3 and 7
+order by r.rno asc;
+
+
+
+        
